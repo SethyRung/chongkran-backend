0e4f6fda7aff987f8ed7f9288eb0b6b9201fe41a
SethyRung <rungsethyhk@gmail.com>
2025-04-26 00:00:03 +0700

    feat: enhance Recipes module with findById method and error handling for recipe retrieval

--- src/recipes/recipes.controller.ts
+++ src/recipes/recipes.controller.ts
@@ -1,8 +1,24 @@
-import { Body, Controller, Get, Post } from "@nestjs/common";
-import { ApiBearerAuth, ApiOkResponse, ApiTags } from "@nestjs/swagger";
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+  Query,
+} from "@nestjs/common";
+import {
+  ApiBearerAuth,
+  ApiOkResponse,
+  ApiQuery,
+  ApiTags,
+} from "@nestjs/swagger";
 import { RecipesService } from "./recipes.service";
 import { RecipeDto } from "./dto/recipe.dto";
 import { CreateRecipeDto } from "./dto/create_recipe.dto";
+import { Role } from "src/common/enums/role.enum";
+import { Roles } from "src/common/decorators/roles.decorator";
 
 @ApiTags("Recipes")
 @Controller("recipes")
@@ -17,9 +33,29 @@ export class RecipesController {
   }
 
   @ApiBearerAuth()
+  @Get("/:id")
+  @ApiOkResponse({ type: RecipeDto })
+  async findById(@Param("id") id: string): Promise<RecipeDto> {
+    return this.recipesService.findById(id);
+  }
+
+  @ApiBearerAuth()
   @Post()
   @ApiOkResponse({ type: RecipeDto })
   async create(@Body() createRecipe: CreateRecipeDto): Promise<RecipeDto> {
     return await this.recipesService.create(createRecipe);
+  }
+
+  @ApiBearerAuth()
+  @Roles(Role.Admin)
+  @Put("/update-status")
+  @ApiQuery({ name: "id", type: String })
+  @ApiQuery({ name: "status", enum: ["pending", "approved", "rejected"] })
+  @ApiOkResponse({ type: RecipeDto })
+  async updateStatus(
+    @Query("id") id: string,
+    @Query("status") status: RecipeDto["status"]
+  ): Promise<RecipeDto> {
+    return await this.recipesService.updateStatus(id, status);
   }
 }
--- src/recipes/recipes.service.ts
+++ src/recipes/recipes.service.ts
@@ -1,4 +1,10 @@
-import { Injectable } from "@nestjs/common";
+import {
+  ForbiddenException,
+  HttpException,
+  HttpStatus,
+  Injectable,
+  NotFoundException,
+} from "@nestjs/common";
 import { RecipeDto } from "./dto/recipe.dto";
 import { CreateRecipeDto } from "./dto/create_recipe.dto";
 import { InjectModel } from "@nestjs/mongoose";
@@ -22,20 +28,62 @@ export class RecipesService {
     }));
   }
 
-  async create(createRecipe: CreateRecipeDto): Promise<RecipeDto> {
-    const currentDate = new Date().toISOString();
+  async findById(id: string): Promise<RecipeDto> {
+    const recipe = await this.recipeModel.findById(id).exec();
 
-    const recipe = await this.recipeModel.create({
-      ...createRecipe,
-      createdAt: currentDate,
-      updatedAt: currentDate,
-    });
+    if (!recipe)
+      throw new HttpException("Recipe not found.", HttpStatus.BAD_REQUEST);
 
     return {
       ...recipe.toObject(),
       id: recipe._id.toString(),
       author: recipe.author.toString(),
       category: recipe.category.toString(),
+    };
+  }
+
+  async create(createRecipe: CreateRecipeDto): Promise<RecipeDto> {
+    const currentDate = new Date().toISOString();
+
+    const created = await this.recipeModel.create({
+      ...createRecipe,
+      createdAt: currentDate,
+      updatedAt: currentDate,
+    });
+
+    return {
+      ...created.toObject(),
+      id: created._id.toString(),
+      author: created.author.toString(),
+      category: created.category.toString(),
+    };
+  }
+
+  async updateStatus(
+    recipeId: string,
+    status: RecipeDto["status"]
+  ): Promise<RecipeDto> {
+    const currentDate = new Date().toISOString();
+
+    const updated = await this.recipeModel
+      .findByIdAndUpdate(
+        recipeId,
+        { $set: { status: status, updateAt: currentDate } },
+        { new: true }
+      )
+      .exec();
+
+    if (!updated)
+      throw new HttpException(
+        "Recipe not found. Please check the information and try again.",
+        HttpStatus.BAD_REQUEST
+      );
+
+    return {
+      ...updated.toObject(),
+      id: updated._id.toString(),
+      author: updated.author.toString(),
+      category: updated.category.toString(),
     };
   }
 }
