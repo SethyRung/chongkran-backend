c65f5f80a39f0633e7faadd78990d9297fdb6ee6
SethyRung <rungsethyhk@gmail.com>
2025-04-29 21:16:04 +0700

    refactor(recipes): update Recipes schema and update RecipesModule to use it

--- src/recipes/recipes.module.ts
+++ src/recipes/recipes.module.ts
@@ -2,13 +2,13 @@ import { Module } from "@nestjs/common";
 import { RecipesController } from "./recipes.controller";
 import { RecipesService } from "./recipes.service";
 import { MongooseModule } from "@nestjs/mongoose";
-import { Recipes, RecipesSchema } from "./schemas/recipes.schema";
+import { Recipe, RecipeSchema } from "./schemas/recipe.schema";
 import { User, UserSchema } from "src/user/schemas/user.schema";
 
 @Module({
   imports: [
     MongooseModule.forFeature([
-      { name: Recipes.name, schema: RecipesSchema },
+      { name: Recipe.name, schema: RecipeSchema },
       { name: User.name, schema: UserSchema },
     ]),
   ],
